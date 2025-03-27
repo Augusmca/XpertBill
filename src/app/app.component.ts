4bfb0855aa0a5f2c7b694f4166a2a69183c43bc1
Augustin Victor <Augustin.Victor@navistar.com>
2025-03-28 02:32:58 +0530

    dist folder removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'XpertInvoice';
+  title = 'Xpert Invoice - New Title';
 }
